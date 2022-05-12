7c4db11880eb59a1cab6a6516cc5712cd70ca2e6
Adam Januszewski <Adpjan@att.net>
2022-05-12 10:18:19 -0400

    Initial Commit

---
+++ AvocadoEats/AvocadoEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoEatsApp.swift
+//  AvocadoEats
+//
+//  Created by adam janusewski on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
