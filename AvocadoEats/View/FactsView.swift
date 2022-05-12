9dc71be758a518256c0afbb32e4747c319c80cb6
Adam Januszewski <Adpjan@att.net>
2022-05-12 15:14:10 -0400

    Added new View, and table

---
+++ AvocadoEats/View/FactsView.swift
@@ -0,0 +1,21 @@
+//
+//  FactsView.swift
+//  AvocadoEats
+//
+//  Created by adam janusewski on 5/12/22.
+//
+
+import SwiftUI
+
+struct FactsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FactsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FactsView()
+            .previewLayout(.fixed(width: 400, height: 220))
+    }
+}
